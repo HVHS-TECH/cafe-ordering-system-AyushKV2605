e2791c754409337e73cef9cb5e8f21efe6f532c9
AyushKV2605 <24249ak@hvhs.school.nz>
2026-06-18 21:38:31 +0000

    Jun 18, 2026, 9:38 PM

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log
+console.log("Final-Website");
+
+  const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+    // get values from the form
+  const NAME_FIELD = document.getElementById("nameField");
+
+  
+  let username = NAME_FIELD.value;
+
